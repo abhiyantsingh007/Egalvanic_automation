ff9a0c8862aed192aa76d217908b07c34fc8ce08
abhiyantsingh007 <abhiyantsingh007@gmail.com>
2025-12-17 13:53:59 +0530

    Fix IOSLaunchTest.java dependencies and add runner scripts

--- src/main/java/IOSLaunchTest.java
+++ src/main/java/IOSLaunchTest.java
@@ -2,80 +2,67 @@ import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.IOSMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
-public class iOS {  // Changed class name to match file name
+public class IOSLaunchTest {
 
-    private IOSDriver driver;
+    public static void main(String[] args) {
 
-    @BeforeClass
-    public void setUp() throws MalformedURLException {
+        IOSDriver driver = null;
 
-        DesiredCapabilities caps = new DesiredCapabilities();
+        try {
+            DesiredCapabilities caps = new DesiredCapabilities();
 
-        // ===============================
-        // Device Information
-        // ===============================
-        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
-        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "iPad");
-        caps.setCapability(MobileCapabilityType.UDID, "00008103-001E71EA147B001E");
-        caps.setCapability("platformVersion", "17.0"); // update if different
+            // Device details
+            caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+            caps.setCapability(MobileCapabilityType.DEVICE_NAME, "iPad");
+            caps.setCapability(MobileCapabilityType.UDID, "00008103-001E71EA147B001E");
+            caps.setCapability("platformVersion", "17.0"); // change if needed
 
-        // ===============================
-        // App Information
-        // ===============================
-        caps.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.egalvanic.zplatform-QA");
+            // App to open
+            caps.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.egalvanic.zplatform-QA");
 
-        // ===============================
-        // Automation
-        // ===============================
-        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+            // Automation engine
+            caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
 
-        // ===============================
-        // Xcode / WDA Signing (MANDATORY)
-        // ===============================
-        caps.setCapability("xcodeOrgId", "YOUR_TEAM_ID_HERE"); // <-- add from Xcode
-        caps.setCapability("xcodeSigningId", "iPhone Developer");
-        caps.setCapability("updatedWDABundleId", "com.abhiyant.wdarunner");
+            // Xcode / WDA signing
+            caps.setCapability("xcodeOrgId", "YOUR_TEAM_ID_HERE");
+            caps.setCapability("xcodeSigningId", "iPhone Developer");
+            caps.setCapability("updatedWDABundleId", "com.abhiyant.wdarunner");
 
-        // ===============================
-        // WebDriverAgent Settings
-        // ===============================
-        caps.setCapability("useNewWDA", true);
-        caps.setCapability("wdaLaunchTimeout", 300000);
-        caps.setCapability("wdaConnectionTimeout", 300000);
+            // Additional capabilities for stability
+            caps.setCapability("useNewWDA", true);
+            caps.setCapability("wdaLaunchTimeout", 300000);
+            caps.setCapability("wdaConnectionTimeout", 300000);
+            caps.setCapability(MobileCapabilityType.NO_RESET, false);
 
-        // ===============================
-        // App State
-        // ===============================
-        caps.setCapability(MobileCapabilityType.NO_RESET, false);
+            // Launch app
+            driver = new IOSDriver(
+                    new URL("http://127.0.0.1:4723/wd/hub"),
+                    caps
+            );
 
-        // ===============================
-        // Start Driver
-        // ===============================
-        driver = new IOSDriver(
-                new URL("http://127.0.0.1:4723/wd/hub"),
-                caps
-        );
+            // Set implicit wait
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
+            System.out.println("App launched successfully.");
 
-    @Test
-    public void launchAppTest() {
-        System.out.println("Z Platform QA app launched successfully on iPad.");
-    }
+            // Keep app open for few seconds
+            Thread.sleep(5000);
 
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
+        } catch (MalformedURLException e) {
+            System.err.println("Invalid URL provided for Appium server");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Error during iOS app launch");
+            e.printStackTrace();
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
         }
     }
 }
